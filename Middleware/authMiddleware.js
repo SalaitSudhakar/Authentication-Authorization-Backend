bcf48048eb602f28450cb95fe657724b68a731a6
SalaitSudhakar <salaitsudhakar.s@gmail.com>
2025-03-28 19:49:30 +0530

    modified

--- Middleware/authMiddleware.js
+++ Middleware/authMiddleware.js
@@ -4,13 +4,19 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const authMiddleware = async (req, res, next) => {
-    const {token} = req.cookies;
-
-    if (!token){
-        return res.status(401).json({success: false, message: 'Token is missing'});
-    }
 
     try {
+
+        if (!req.cookies || !req.cookies.token){
+            return res.status(401).json({success: false, message: 'Token is missing'})
+        }
+
+        const token = req.cookies.token;
+
+        if (!process.env.JWT_SECRET) {
+            return res.status(500).json({success: false, message: 'JWT_SECRET is Missing'})
+        }
+        
        const tokenDecoded = jwt.verify(token, process.env.JWT_SECRET);
        
        if (tokenDecoded.id){
